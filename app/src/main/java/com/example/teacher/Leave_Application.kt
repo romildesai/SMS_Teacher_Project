7ea92eabc14241e516f31bf7409facd3d43f828e
romildesai <kishandesai479@gmail.com>
2023-01-07 21:05:42 +0530

    today third commit

---
+++ app/src/main/java/com/example/teacher/Leave_Application.kt
@@ -0,0 +1,11 @@
+package com.example.teacher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Leave_Application : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leave_application)
+    }
+}
