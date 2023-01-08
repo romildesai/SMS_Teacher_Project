3927579e86c62c9401598ee22c8acb78b17be7c5
romildesai <kishandesai479@gmail.com>
2023-01-08 21:35:32 +0530

    second commit

---
+++ app/src/main/java/com/example/teacher/Student_List.kt
@@ -0,0 +1,11 @@
+package com.example.teacher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Student_List : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_list)
+    }
+}
