e5eb121e899c853c7f4efc8a0ee91ae604c4525c
hinalidhameliya <hinalidhameliya@gmail.com>
2023-01-09 23:24:14 +0530

    commited by hinu

---
+++ app/src/main/java/com/example/teacher/activity_edit_profile.kt
@@ -0,0 +1,11 @@
+package com.example.teacher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_edit_profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
