83f82a306e5aac2d4f199297f72590bff4168f18
hinalidhameliya <hinalidhameliya@gmail.com>
2023-01-17 10:16:26 +0530

    commited by hini

---
+++ app/src/main/java/com/example/teacher/Activity_edit_teacher_profile.java
@@ -0,0 +1,14 @@
+package com.example.teacher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_edit_teacher_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_teacher_profile);
+    }
+}
