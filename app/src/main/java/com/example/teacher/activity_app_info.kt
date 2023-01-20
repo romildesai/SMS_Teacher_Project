d1a85bb1e2e6deb736b238d6581cdf911aa21fe9
hinalidhameliya <hinalidhameliya@gmail.com>
2023-01-20 22:46:18 +0530

    hini commited today

---
+++ app/src/main/java/com/example/teacher/activity_app_info.kt
@@ -0,0 +1,11 @@
+package com.example.teacher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_app_info : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_info)
+    }
+}
---
+++ app/src/main/java/com/example/teacher/activity_student_details.kt
@@ -0,0 +1,11 @@
+package com.example.teacher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_student_details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_details)
+    }
+}
