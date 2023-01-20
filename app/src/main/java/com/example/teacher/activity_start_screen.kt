397aca46d48170dace8404300aec970987f9aedf
hinalidhameliya <hinalidhameliya@gmail.com>
2023-01-20 12:47:39 +0530

    hini commited

---
+++ app/src/main/java/com/example/teacher/activity_start_screen.kt
@@ -0,0 +1,11 @@
+package com.example.teacher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_start_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_screen)
+    }
+}
