8f8148e67561d69c625059c05a548731eb03e91d
romildesai <kishandesai479@gmail.com>
2023-01-22 10:58:14 +0530

    today commit by romil

--- app/src/main/java/com/example/teacher/activity_add_student.kt
+++ app/src/main/java/com/example/teacher/activity_add_student.kt
@@ -1,22 +1,25 @@
 package com.example.teacher
 
-import android.app.PendingIntent.getActivity
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+
 class activity_add_student : AppCompatActivity() {
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_student)
-
         supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.parseColor("#014f86")))
-        title = "ADD STUDENT";
+
 
     }
-
-
-
 }
+
+
+
