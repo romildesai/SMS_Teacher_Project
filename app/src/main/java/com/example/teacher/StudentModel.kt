3c43594741d8f2b0e591f93299504e8c18ea52ee
romildesai <kishandesai479@gmail.com>
2023-02-02 14:01:59 +0530

    today

---
+++ app/src/main/java/com/example/teacher/StudentModel.kt
@@ -0,0 +1,16 @@
+package com.example.teacher
+
+class StudentModel(
+
+    var fullname:String? = null,
+    var enrollment:String? = null,
+    var email:String? = null,
+    var pass:String? = null,
+    var gender:String?= null,
+    var bob:String?= null,
+    var phone:String?= null,
+    var semester:String?=null,
+    var cls:String?=null,
+    var branch:String?=null,
+    var fees:String?=null
+)
